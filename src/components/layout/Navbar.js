59cd529ec4ba51883361029e6c259a922e4c8dbe
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-13 01:21:42 +0300

    Add defaultProps & propTypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,25 @@
 /* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/static-property-placement */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+  };
+
+  static propTypes = {
+    title: PropTypes.string,
+  };
+
   render() {
+    const { title } = this.props;
     return (
-      <nav className="navbar bg-primary">
-        <h1>Navbar</h1>
+      <nav className="navbar bg-success">
+        <h1>
+          <i className="fab fa-github m-1" />
+          {title}
+        </h1>
       </nav>
     );
   }
