c2c188076af36efea82d41d607e12255f359a4e7
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-13 01:02:05 +0300

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable import/no-named-as-default */
+
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello React</h1>
+        <Navbar />
       </div>
     );
   }
