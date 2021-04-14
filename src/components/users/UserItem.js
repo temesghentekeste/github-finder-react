1734adfe5957c24f0e63f8cefadb8b9f39d1bc05
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-14 10:52:21 +0300

    Update UserItem comp.  - convert it to functional component  - add prop-types  - destructure attributes and render them

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,33 +1,26 @@
-/* eslint-disable camelcase */
-import React, { Component } from 'react';
+/* eslint-disable  camelcase */
+/* eslint-disable  react/prop-types */
+import { PropTypes } from 'prop-types';
 
-export default class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    };
-  }
+const UserItem = ({ user: { login, avatar_url, html_url } }) => (
+  <div className="card text-center">
+    <img
+      src={avatar_url}
+      alt={login}
+      className="round-img"
+      style={{ width: '60px' }}
+    />
+    <h2>{login}</h2>
+    <div>
+      <a href={html_url} className="btn btn-dark btn-sm my-1">
+        More
+      </a>
+    </div>
+  </div>
+);
 
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt={login}
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h2>{login}</h2>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
-      </div>
-    );
-  }
-}
+UserItem.prototype = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
