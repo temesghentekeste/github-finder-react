58fad45dbca193d0bf94b3080cc43fd294f5fa96
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-19 15:58:44 +0300

    Change Search component to class component; add text state to the component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import { Component } from 'react';
 
-const Search = () => (
-  <div>
-    <form className="form">
-      <input type="text" name="text" placeholder="Search user..." />
-      <input type="submit" value="Search" className="btn btn-dark btn-block" />
-    </form>
-  </div>
-);
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  render() {
+    const { text } = this.state;
+    return (
+      <div>
+        <form className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search user..."
+            value={text}
+            onChange={(e) => this.setState({ text: e.target.value })}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Search;
