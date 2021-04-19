56613b50ed5168f3c3b97c94c2e42f114a2388c2
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-20 00:29:24 +0300

    Add about page and its route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
+import About from './components/pages/About';
 
 class App extends Component {
   constructor() {
@@ -101,6 +102,7 @@ class App extends Component {
                 </>
               )}
             />
+            <Route exact path="/about" component={About} />
           </Switch>
         </div>
       </Router>
