39fa0d7b94e179cd3ce7ad1505d3f8f9e59887fe
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-04-21 01:48:47 +0300

    Update User comp. fetch user on componentDidMount and distructure user params

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,47 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/static-property-placement */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable camelcase */
+/* eslint-disable  no-unused-vars */
 
-const User = () => (
-  <div>
-    <h1>User</h1>
-  </div>
-);
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class User extends Component {
+  static propTypes = {
+    getUser: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    const { getUser } = this.props;
+    const { login } = this.props.match.params;
+    getUser(login);
+  }
+
+  render() {
+    const { user, loading } = this.props;
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hereable,
+    } = user;
+
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+}
 
 export default User;
